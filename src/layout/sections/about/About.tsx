24192d8bcad14671ab479d117f011ef44478695f
Aleksei Korolyov <a97415393@gmail.com>
2026-04-18 19:19:19 +0300

    portfolio about

---
+++ src/layout/sections/about/About.tsx
@@ -0,0 +1,15 @@
+
+
+export const About = () => {
+    return (
+        <div>
+            <div>
+                <span>Hi 👋,</span>
+                <span>My name is</span>
+                <span>Pavan MG</span>
+                <h1>I build things for web</h1>
+            </div>
+        </div>
+    );
+};
+
