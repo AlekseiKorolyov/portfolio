bd6061f3a88eff7a4dcc51ab8621246739c818c3
Aleksei Korolyov <a97415393@gmail.com>
2026-04-19 20:21:29 +0300

    portfolio styled

---
+++ src/layout/styles/Theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+    colors: {
+        primaryBg: "#191919",
+        font: "#a7a7a7",
+        fontGr: "linear-gradient(19.493042368983197deg, #00c0fd 0%, #e70faa 100%)"
+    }
+}
